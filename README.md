90799df117424459139b0cfea52c06cc4cc7a361
mjchoi <mjchoi@ex-em.com>
2021-05-29 21:14:27 +0900

    [queue] princess

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - [괄호 문자 제거](stack-queue/2-remove_parenthesis_string.js)
 - [후위식 연산](stack-queue/4-postfix.js)
 - [쇠막대기](stack-queue/5-iron-bars.js)
+- [공주 구하기](stack-queue/6-princess.js)
---
+++ stack-queue/6-princess.js
@@ -0,0 +1,27 @@
+function solution([n, k]) {
+  let princes = Array.from({ length: n }, (v, i) => i + 1);
+  const convertK = k - 1;
+  let temp = [];
+  let turn = 0;
+
+  while(princes.length > 1) {
+    princes.forEach(() => {
+      temp = princes.shift();
+      if (turn === convertK) {
+        turn = 0;
+      } else {
+        princes.push(temp);
+        turn++;
+      }
+    })
+  }
+
+  console.log(princes[0]);
+  return princes[0];
+}
+
+// Execute Test
+const exampleStrList = [
+  [8, 3],
+];
+exampleStrList.forEach(exampleStr => solution(exampleStr));
