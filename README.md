a6a2576ca824d01e33a3a46a149237073918bfbd
mjchoi <mjchoi@ex-em.com>
2021-05-30 15:36:27 +0900

    [stack] crane

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Stack and Queue
 - [올바른 괄호](stack-queue/1-correct-parenthesis.js)
 - [괄호 문자 제거](stack-queue/2-remove_parenthesis_string.js)
+- [크레인 인형 뽑기](stack-queue/3-crane.js)
 - [후위식 연산](stack-queue/4-postfix.js)
 - [쇠막대기](stack-queue/5-iron-bars.js)
 - [공주 구하기](stack-queue/6-princess.js)
@@ -11,3 +12,13 @@
 ## Soring and Greedy and Decision
 - [선택 정렬](sorting-greedy-decision/1-selection-sort.js)
 - [버블 정렬](sorting-greedy-decision/2-bubble-sort.js)
+- Special Sort(구글 인터뷰)
+- 삽입 정렬
+- Least Recently Used(카카오 캐시 문제 변형)
+- 장난꾸러기 현수
+- 좌표 정렬
+- 회의실 배정
+- 결혼식
+- 이분 검색
+- 뮤직비디오(결정 알고리즘)
+- 마구간 정하기(결정 알고리즘)
---
+++ stack-queue/3-crane.js
@@ -0,0 +1,40 @@
+function solution({ board, moves }) {
+  const stack = [];
+  let burst = 0;
+
+  let tempItem;
+  moves.forEach(mItem => {
+    board.some(bItem => {
+      tempItem = bItem[mItem - 1];
+      if (tempItem) {
+        stack.push(tempItem);
+        bItem[mItem - 1] = 0;
+
+        if (tempItem === stack[stack.length - 1]) {
+          burst += 2;
+          stack.pop();
+          stack.pop();
+        }
+      }
+      return tempItem;
+    })
+  })
+
+  console.log(stack, burst);
+  return burst;
+}
+
+// Execute Test
+const exampleStrList = [
+  {
+    board: [
+      [0,0,0,0,0],
+      [0,0,1,0,3],
+      [0,2,5,0,1],
+      [4,2,4,4,2],
+      [3,5,1,3,1]
+    ], //board 배열
+    moves: [1,5,3,5,1,2,1,4], //moves 배열
+  },
+];
+exampleStrList.forEach(exampleStr => solution(exampleStr));
