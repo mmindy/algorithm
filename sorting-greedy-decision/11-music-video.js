05380faa9b007b42b7f61be88308fa01a1e31a2b
mjchoi <mjchoi@ex-em.com>
2021-06-28 21:23:09 +0900

    [decision] music video

--- sorting-greedy-decision/11-music-video.js
+++ sorting-greedy-decision/11-music-video.js
@@ -1,41 +1,50 @@
+/** 뮤직비디오 (결정 알고리즘) - 이분검색 응용
+ * right: 전체 곡의 시간합
+ * left: 가장 긴 곡의 시간
+ * > 이분 검색 통해서 dvd 시간이 mid일 경우, dvd가 몇개 되는지 판단
+ * > 주어진 숫자가 될 때까지 loop 후 mid 시간 반환
+ */
+
+
 function solution([num, dvdNum, songs]) {
   songs = songs.split(' ').map(item => +item);
   const total = songs.reduce((c, p) => c + p, 0);
+  const getMid = (l, r) => parseInt((l + r) / 2);
 
   if (total > 10000) {
     return;
   }
 
-  const dvdTime = Math.ceil(total / dvdNum);
+  const getCount = (time, arr) => {
+    let [sum, cnt] = [0, 1];
 
-  const getDvdTime = (time) => {
-    const dvds = [];
-    for (let i = 0; i < dvdNum; i++) {
-      dvds[i] = [];
-    }
-    let sum = 0;
-    let song;
-    const tmpSongs = [...songs];
-    dvds.forEach(arr => {
-      while (tmpSongs.length > 0) {
-        if (sum + tmpSongs[0] <= time) {
-          song = tmpSongs.shift();
-          sum += song;
-          arr.push(song)
-        } else {
-          break;
-        }
+    for (let min of arr) {
+      if (sum + min > time) {
+        cnt++;
+        sum = min;
+      } else {
+        sum += min;
       }
-      sum = 0;
-    })
-
-    if (tmpSongs.length) {
-      return getDvdTime(time + 1);
     }
-    return time;
+    return cnt;
   }
 
-  return getDvdTime(dvdTime);
+  let [left, right] = [Math.max(...songs), total];
+  let [count, mid] = [1, 0];
+  while (left <= right) {
+    mid = getMid(left, right);
+    count = getCount(mid, [...songs]);
+
+    if (count > dvdNum) {
+      left = mid + 1;
+    } else if (count < dvdNum) {
+      right = mid - 1;
+    } else {
+      break;
+    }
+  }
+  console.log(mid, count);
+  return mid;
 }
 
 // Execute Test
